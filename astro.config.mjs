55e3466df79c1c8544a634ed8469b99af1df64f9
javierfernandezramos <javierfernandezramos9@gmail.com>
2026-07-11 20:58:08 +0200

    Perfecciones visuales y arreglar bugs del menu y del index

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true,
+  },
   output: 'hybrid',
   adapter: vercel({
     webAnalytics: {
